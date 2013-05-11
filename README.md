e370afe06faea7ec3d9ca6d510a641cc2e20ff60
Matt Whelan <matt@blacklogik.com>
2013-05-11 09:28:24 -0700

    Updated to use actual Baud rates.
    Refactored a tiny bit as well.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Usage: node slowbaud.js interval filenames....
+Usage: node slowbaud.js baudRate filenames....
 
-Interval should be between 50 and 250 for reasonable typing.  Specify as many filenames as you like.
+The baudRate should end up feeling about like a modem connection
+of that rate.  Characters per second is 1/9 of that rate (we assume
+no parity, 8 data bits and 1 stop bit).  If you want something more
+like a fast, sustained typing speed, go for 70-100.
+
+Specify as many filenames as you like.  They should, of course, but plain text.  
--- slowbaud.js
+++ slowbaud.js
@@ -1,26 +1,42 @@
 var SlowTransform = require("./slowtransform"),
 	fs = require("fs");
 
-var args = process.argv.slice(2);
-var interval = parseInt(args.shift());
+function SlowBaud(baudRate, filenames){
+	var charRate = baudRate / 9.0; //N,8,1
+	this.interval = 1000 / charRate;
+	this.filenames = filenames;
+}
 
-function printFile(name, next){
-	process.stdout.write(name + ":\n\n");
-	var st = new SlowTransform(interval);
+SlowBaud.prototype.printFile = function printFile(name, next){
+	process.stdout.write("\n\n-----------------------------------" + name + ":\n\n");
+	var st = new SlowTransform(this.interval);
 
 	st.on("readable", function(){
 		process.stdout.write(st.read());
-	}).on("end", next);
+	}).on("end", function(){
+		next();
+	});
 
 	fs.readFile(name, function(err, data){
 		st.write(data);
 	});
 }
 
-function processArgs(){
-	if(args.length){
-		printFile(args.shift(), processArgs);
+SlowBaud.prototype.processFiles = function processFiles(){
+	if(this.filenames.length){
+		this.printFile(this.filenames.shift(), this.processFiles.bind(this));
 	}
 }
 
-processArgs();
+if(require.main == module){
+	if(process.argv.length < 4){
+		process.stderr.write("Usage: node slowbaud <baudRate> <file> [files...]\n");
+		process.exit(1);
+	}
+	var args = process.argv.slice(2),
+		baudRate = parseInt(args.shift());
+
+	var sb = new SlowBaud(baudRate, args);
+	sb.processFiles();
+	process.stdout.write("\n");
+}
